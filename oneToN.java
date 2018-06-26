6978dc845d0f7e614229afd0b3bc1d6f9382ef6c
Vishal4508 <40260749+Vishal4508@users.noreply.github.com>
2018-06-26 12:30:18 +0530

    Update oneToN.java

--- oneToN.java
+++ oneToN.java
@@ -12,11 +12,14 @@ public static void main(String[] args) {
 	
 	
 }*/
-	public static void main(String[] args) {
-	int inputnum = 5;	
-	int i;
-	for (i = 0; i<inputnum ; i++) {
-		System.out.println(i);
-	}
+	
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		System.out.print("print numbers upto :");
+		int i = scan.nextInt();
+		int j = 1;
+		for (j =1; j <= i; j++ ){
+			System.out.println(j);
+		}
 	}		
 }
