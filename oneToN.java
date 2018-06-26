df9a5234c3416b6d3f9d9dc4d6ed46c38f3f8582
vishalQA4508 <40446555+vishalQA4508@users.noreply.github.com>
2018-06-26 07:54:24 +0530

    Add files via upload

---
+++ oneToN.java
@@ -0,0 +1,22 @@
+package practice2;
+
+public class oneToN {
+/*	static int p;
+	public int getUserInput(int q) {
+	p = q;
+	return p;
+	}
+	
+public static void main(String[] args) {
+	System.out.println("entered number is :"+p);
+	
+	
+}*/
+	public static void main(String[] args) {
+	int inputnum = 5;	
+	int i;
+	for (i = 0; i<inputnum ; i++) {
+		System.out.println(i);
+	}
+	}		
+}
---
+++ tableOfN.java
@@ -0,0 +1,18 @@
+package practice2;
+
+public class tableOfN {
+	public static void main(String[] args) {
+		
+		
+		int tableOf = 16;//change this value
+		
+		
+		int i = tableOf;
+		int j;
+		for(j =1; j<=10; j++) {
+			System.out.println(i+"X"+j+ "= " +i*j);
+		}
+		
+	}
+
+}
